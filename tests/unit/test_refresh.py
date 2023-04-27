22d76104700a419037f32d4c1191be102c724ba1
jackwotherspoon <jackwoth@google.com>
2023-04-27 22:26:30 +0000

    chore: make client a fixture

--- tests/unit/test_refresh.py
+++ tests/unit/test_refresh.py
@@ -12,10 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 from typing import Any
 
-from aiohttp import web
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from mocks import FakeCredentials
@@ -27,41 +25,19 @@ from google.cloud.alloydb.connector.refresh import (
 )
 
 
-async def connectionInfo(request: Any) -> web.Response:
-    response = {
-        "ipAddress": "127.0.0.1",
-        "instanceUid": "123456789",
-    }
-    return web.Response(content_type="application/json", body=json.dumps(response))
-
-
-async def generateClientCertificate(request: Any) -> web.Response:
-    response = {
-        "pemCertificate": "This is the client cert",
-        "pemCertificateChain": [
-            "This is the intermediate cert",
-            "This is the root cert",
-        ],
-    }
-    return web.Response(content_type="application/json", body=json.dumps(response))
-
-
 @pytest.mark.asyncio
-async def test__get_metadata(aiohttp_client: Any, credentials: FakeCredentials) -> None:
+async def test__get_metadata(client: Any, credentials: FakeCredentials) -> None:
     """
     Test _get_metadata returns successfully.
     """
-    app = web.Application()
-    api_version = "v1beta"
-    project = "test-project"
-    region = "test-region"
-    cluster = "test-cluster"
-    name = "test-instance"
-    uri = f"/{api_version}/projects/{project}/locations/{region}/clusters/{cluster}/instances/{name}/connectionInfo"
-    app.router.add_get(uri, connectionInfo)
-    client = await aiohttp_client(app)
     metadata = await _get_metadata(
-        client, "", credentials, project, region, cluster, name
+        client,
+        "",
+        credentials,
+        "test-project",
+        "test-region",
+        "test-cluster",
+        "test-instance",
     )
     assert metadata["ip_address"] == "127.0.0.1"
     assert metadata["uid"] == "123456789"
@@ -69,24 +45,16 @@ async def test__get_metadata(aiohttp_client: Any, credentials: FakeCredentials) 
 
 @pytest.mark.asyncio
 async def test__get_client_certificate(
-    aiohttp_client: Any, credentials: FakeCredentials
+    client: Any, credentials: FakeCredentials
 ) -> None:
     """
     Test _get_client_certificate returns successfully.
     """
-    app = web.Application()
-    api_version = "v1beta"
-    project = "test-project"
-    region = "test-region"
-    cluster = "test-cluster"
     key = rsa.generate_private_key(
         backend=default_backend(), public_exponent=65537, key_size=2048
     )
-    uri = f"/{api_version}/projects/{project}/locations/{region}/clusters/{cluster}:generateClientCertificate"
-    app.router.add_post(uri, generateClientCertificate)
-    client = await aiohttp_client(app)
     certs = await _get_client_certificate(
-        client, "", credentials, project, region, cluster, key
+        client, "", credentials, "test-project", "test-region", "test-cluster", key
     )
     assert certs["client_cert"] == "This is the client cert"
     assert certs["intermediate_cert"] == "This is the intermediate cert"
