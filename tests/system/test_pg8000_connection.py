df5eb843f758d33a484c6ba272f7ff3dcf2ef3d4
jackwotherspoon <jackwoth@google.com>
2023-05-05 15:50:29 +0000

    chore: update test

--- tests/system/test_pg8000_connection.py
+++ tests/system/test_pg8000_connection.py
@@ -21,18 +21,16 @@ import sqlalchemy
 from google.cloud.alloydb.connector import Connector
 
 
-def init_connection_engine() -> sqlalchemy.engine.Engine:
+def init_connection_engine(connector: Connector) -> sqlalchemy.engine.Engine:
     def getconn() -> pg8000.dbapi.Connection:
-        # initialize Connector object for connections to Cloud SQL
-        with Connector() as connector:
-            conn: pg8000.dbapi.Connection = connector.connect(
-                os.environ["ALLOYDB_INSTANCE_URI"],
-                "pg8000",
-                user=os.environ["ALLOYDB_USER"],
-                password=os.environ["ALLOYDB_PASS"],
-                db=os.environ["ALLOYDB_DB"],
-            )
-            return conn
+        conn: pg8000.dbapi.Connection = connector.connect(
+            os.environ["ALLOYDB_INSTANCE_URI"],
+            "pg8000",
+            user=os.environ["ALLOYDB_USER"],
+            password=os.environ["ALLOYDB_PASS"],
+            db=os.environ["ALLOYDB_DB"],
+        )
+        return conn
 
     # create SQLAlchemy connection pool
     pool = sqlalchemy.create_engine(
