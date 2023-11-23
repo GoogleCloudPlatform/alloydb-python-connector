5b72884dbb1cec5fad89bf95a430691f5d3de5c8
Rahul Dubey <100185371+RahulDubey391@users.noreply.github.com>
2023-11-23 23:52:31 +0530

    fix: Removed line

--- google/cloud/alloydb/connector/utils.py
+++ google/cloud/alloydb/connector/utils.py
@@ -50,8 +50,7 @@ def _write_to_file(
 async def generate_keys() -> Tuple[rsa.RSAPrivateKey, str]:
 
     priv_key = rsa.generate_private_key(public_exponent=65537, 
-                                        key_size=2048
-    )
+                                        key_size=2048)
 
     pub_key = (
         priv_key.public_key()
