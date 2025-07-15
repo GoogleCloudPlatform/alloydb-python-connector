5b0f22eb12bc59ae09322db7dd7ec231f8d4b049
rhatgadkar-goog <rhatgadkar@google.com>
2025-07-15 01:27:39 +0000

    check if new pytest-asyncio version is causing the flake

--- google/cloud/alloydbconnector/connector.py
+++ google/cloud/alloydbconnector/connector.py
@@ -383,23 +383,14 @@ class Connector:
                 self.close_async(), loop=self._loop
             )
             # Will attempt to gracefully shut down tasks for 3s
-            print("RISHABH DEBUG: before calling close_async()")
             close_future.result(timeout=3)
-        else:
-            print("RISHABH DEBUG: loop is not running 1")
         # if background thread exists for Connector, clean it up
         if self._thread.is_alive():
             if self._loop.is_running():
                 # stop event loop running in background thread
-                print("RISHABH DEBUG: before calling call_soon_threadsafe()")
                 self._loop.call_soon_threadsafe(self._loop.stop)
-            else:
-                print("RISHABH DEBUG: loop is not running 2")
             # wait for thread to finish closing (i.e. loop to stop)
-            print("RISHABH DEBUG: before calling thread.join()")
             self._thread.join()
-        else:
-            print("RISHABH DEBUG: thread is not alive")
         self._closed = True
 
     async def close_async(self) -> None:
